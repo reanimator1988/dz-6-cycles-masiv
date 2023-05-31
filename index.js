78708679ed73d94e612da1debcea39aeb674bd74
Dmytro <133363330+reanimator1988@users.noreply.github.com>
2023-05-31 21:26:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ alert('Sum is ' + sum)
 const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2']
 let sum = 0
 	for (let i = 0; i < numbers.length; i++) {
-		if (isNaN(numbers[i])) {continue}
+		if (isNaN(numbers[i])) 
+		continue
 	    	sum += Number(numbers[i])
 	}
 alert('Sum is ' + sum)
